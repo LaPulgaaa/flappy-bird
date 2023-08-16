7dcb4c73ff852e86d11215c2ad446f98290a0bbd
Varun Singh <varunsingh9114@gmail.com>
2023-08-16 22:51:10 +0530

    game loop added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const canvas=document.getElementById('canvas');
 const ctx=canvas.getContext("2d");
 
 canvas.height=600;
-canvas.width=800;
+canvas.width=700;
 
 canvas.style.backgroundImage="url('./Assets/background.png')";
 canvas.style.backgroundSize="contain";
@@ -10,7 +10,7 @@ canvas.style.backgroundSize="contain";
 let bird={
     x:20,
     y:100,
-    dx:4,
+    dx:8,
     dy:1,
     acc:3.8,
     u:0,
@@ -19,36 +19,42 @@ let bird={
 let upper1={
     x:200,
     y:0,
-    ux:-30,
+    ux:-10,
+    dx:8,
 
     
 }
 let upper2={
-    x:400,
+    x:450,
     y:0,
-    ux:-30,
+    ux:-10,
+    dx:8,
 }
 let upper3={
-    x:550,
+    x:600,
     y:0,
-    ux:-30,
+    ux:-10,
+    dx:8,
 }
 let lower1={
     x:200,
     y:400,
-    ux:-30,
+    ux:-10,
+    dx:8,
 
 }
 let lower2={
-    x:420,
+    x:445,
     y:350,
-    ux:-30,
+    ux:-10,
+    dx:8,
 
 }
 let lower3={
-    x:550,
+    x:590,
     y:400,
-    ux:-30,
+    ux:-10,
+    dx:8,
 
 }
 //drawing image
@@ -89,12 +95,29 @@ low1.onload=()=>lower1Ready=true;
 const state=(delta)=>{
 
     //moving the pipes 
-    upper1.x+=(upper1.ux)*delta;
-    upper2.x+=upper2.ux*delta;
-    upper3.x+=upper3.ux*delta;
-    lower1.x+=lower1.ux*delta;
-    lower2.x+=lower2.ux*delta;
-    lower3.x+=lower3.ux*delta;
+    upper1.x-=(upper1.dx)
+    upper2.x-=upper2.dx
+    upper3.x-=upper3.dx
+    lower1.x-=lower1.dx
+    lower2.x-=lower2.dx
+    lower3.x-=lower3.dx
+
+    //updating position
+
+    if(upper1.x<=-80)
+    upper1.x=700;
+    if(upper2.x<=-80)
+    upper2.x=700;
+    if(upper3.x<=-80)
+    upper3.x=700;
+    if(lower1.x<=-80)
+    lower1.x=700;
+    if(lower2.x<-80)
+    lower2.x=700;
+    if(lower3.x<-80)
+    lower3.x=700;
+
+    
 
 }
 
@@ -106,6 +129,6 @@ function animate(){
     state(delta)
     update();
 }
-const then=Date.now();
+let then=Date.now();
 animate();
 
